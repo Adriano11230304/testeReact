4b190d2c1e956b8cd45fb5420cf1c6d328d0de37
adriano11230304 <adriano.al.pereira1@gmail.com>
2023-08-17 19:46:41 -0300

    ajustes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,20 @@
 import { Link } from 'react-router-dom';
+import '../../css/style.css'
 
 function Home(){
     return (
-        <div>
-            <h1>Home</h1>
-            <nav>
-                <Link to="/">
-                    <h2>Acessar Home</h2>
-                </Link>
-                <Link to="/usuarios">
-                    <h2>Acessar Usuários</h2>
-                </Link>
-            </nav>
+        <div className='row'>
+            <div className='navbar-nav navbar-right mt-2 mb-2 ml-5'>
+                <h1>Home</h1>
+                <nav >
+                    <Link to="/">
+                        <h2>Acessar Home</h2>
+                    </Link>
+                    <Link to="/usuarios">
+                        <h2>Acessar Usuários</h2>
+                    </Link>
+                </nav>
+            </div>
         </div>
     )
 }
